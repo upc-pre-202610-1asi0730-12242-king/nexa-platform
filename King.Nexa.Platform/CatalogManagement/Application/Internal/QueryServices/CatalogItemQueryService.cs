55a4101a4b0a5d81a70f98a6c67c4cff00290e20
R0obxdnt <u202416289@upc.edu.pe>
2026-06-08 19:12:05 -0500

    feat(catalog): queries
    Context:
    - Branch: feature/catalog-queries
    - Scope: Bounded Context catalog
    
    Changes:
    - Added: 4 files
    - Modified: 2 files
    - Deleted: 0 files
    
    Reason:
    - Implementing queries for bounded context catalog.
    
    Validation:
    - Verified code structure and path alignment.

--- King.Nexa.Platform/CatalogManagement/Application/Internal/QueryServices/CatalogItemQueryService.cs
+++ King.Nexa.Platform/CatalogManagement/Application/Internal/QueryServices/CatalogItemQueryService.cs
@@ -2,6 +2,7 @@ using King.Nexa.Platform.CatalogManagement.Application.CommandServices;
 using King.Nexa.Platform.CatalogManagement.Application.QueryServices;
 using King.Nexa.Platform.CatalogManagement.Domain.Model.Aggregates;
 using King.Nexa.Platform.CatalogManagement.Domain.Model.Queries;
+using King.Nexa.Platform.CatalogManagement.Domain.Model.ValueObjects;
 using King.Nexa.Platform.CatalogManagement.Domain.Repositories;
 
 namespace King.Nexa.Platform.CatalogManagement.Application.Internal.QueryServices;
@@ -22,4 +23,16 @@ public class CatalogItemQueryService(ICatalogItemRepository catalogItemRepositor
     /// </summary>
     public async Task<CatalogItem?> Handle(GetCatalogItemByIdQuery query, CancellationToken cancellationToken = default) =>
         await catalogItemRepository.FindByIdAsync(query.Id, cancellationToken);
+
+    public async Task<CatalogItem?> Handle(GetCatalogItemByCatalogItemIdQuery query, CancellationToken cancellationToken = default) =>
+        await catalogItemRepository.FindByCatalogItemIdAsync(new CatalogItemId(query.CatalogItemId), cancellationToken);
+
+    public async Task<IEnumerable<CatalogItem>> Handle(GetCatalogItemsByCategoryNameQuery query, CancellationToken cancellationToken = default) =>
+        await catalogItemRepository.ListByCategoryNameAsync(new CategoryName(query.CategoryName), cancellationToken);
+
+    public async Task<IEnumerable<CatalogItem>> Handle(GetCatalogItemsByBrandNameQuery query, CancellationToken cancellationToken = default) =>
+        await catalogItemRepository.ListByBrandNameAsync(new BrandName(query.BrandName), cancellationToken);
+
+    public async Task<IEnumerable<CatalogItem>> Handle(GetCatalogItemsByColdChainRequirementQuery query, CancellationToken cancellationToken = default) =>
+        await catalogItemRepository.ListByColdChainRequirementAsync(query.ColdChainRequirement, cancellationToken);
 }

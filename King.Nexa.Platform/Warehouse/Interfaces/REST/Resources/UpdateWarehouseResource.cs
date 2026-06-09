d088ea7aefbd90a7d028bdf6e942ea6ec7cfdfcc
Diego Y. Sandoval <diego64g284@gmail.com>
2026-06-09 00:06:40 -0500

    refactor(warehouse): infrastructure
    Context:
    - Branch: feature/warehouse-infrastructure
    - Scope: Bounded Context warehouse
    
    Changes:
    - Added: 0 files
    - Modified: 0 files
    - Deleted: 30 files
    
    Reason:
    - Implementing infrastructure for bounded context warehouse.
    
    Validation:
    - Verified code structure and path alignment.

--- King.Nexa.Platform/Warehouse/Interfaces/REST/Resources/UpdateWarehouseResource.cs
+++
@@ -1,3 +0,0 @@
-namespace King.Nexa.Platform.Warehouse.Interfaces.Rest.Resources;
-
-public record UpdateWarehouseResource(string Name, string Location, decimal MinimumTemperature, decimal MaximumTemperature);

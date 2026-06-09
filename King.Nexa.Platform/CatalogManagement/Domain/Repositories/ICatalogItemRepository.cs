c60af0c9d3d5cdc405c6e78e89367c09c4312984
R0obxdnt <u202416289@upc.edu.pe>
2026-06-08 19:20:25 -0500

    refactor(catalog): infrastructure
    Context:
    - Branch: feature/catalog-infrastructure
    - Scope: Bounded Context catalog
    
    Changes:
    - Added: 1 files
    - Modified: 3 files
    - Deleted: 0 files
    
    Reason:
    - Implementing infrastructure for bounded context catalog.
    
    Validation:
    - Verified code structure and path alignment.

--- King.Nexa.Platform/CatalogManagement/Domain/Repositories/ICatalogItemRepository.cs
+++ King.Nexa.Platform/CatalogManagement/Domain/Repositories/ICatalogItemRepository.cs
@@ -7,4 +7,10 @@ namespace King.Nexa.Platform.CatalogManagement.Domain.Repositories;
 public interface ICatalogItemRepository : IBaseRepository<CatalogItem>
 {
     Task<CatalogItem?> FindByCatalogItemIdAsync(CatalogItemId catalogItemId, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CatalogItem>> ListByCategoryNameAsync(CategoryName categoryName, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CatalogItem>> ListByBrandNameAsync(BrandName brandName, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CatalogItem>> ListByColdChainRequirementAsync(ColdChainRequirement coldChainRequirement, CancellationToken cancellationToken = default);
 }
